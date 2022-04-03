97ba9419eb10c51cb00c0289e05c50fe2391c67c
perfecttennails <bgc.info@gmail.com>
2022-04-03 22:19:12 +0000

    add the begining av js

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+let questions = [
+    {
+question: #1 How much percentage of the human body is constituted by water?,
+answers: {
+    A: '35%',
+    B: '45%',
+    C: '60%',
+    D: '90%'
+},
+correctAnswer: '60%' 
+},
+{
+question: #2 Is the nose of an average human being as long as the length of their thumb?
+answer: {
+    A:'Yes',
+    B:'No',
+    C:'Half the thumb',
+    D:'A thumb and a half'
+},
+correctAnswer:'Yes'
+'
