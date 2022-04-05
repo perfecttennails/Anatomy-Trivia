bef88a733fa58664d5175e389a7701e90d3f7423
perfecttennails <bgc.info@gmail.com>
2022-04-05 02:03:00 +0000

    add the function to start button in js

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 # Anatomy-Trivia
 
-This puzzle is for all ages.
- 30+ Questions About The Gross, Coolest, And Craziest Facts About The Human Body.
+ View the live site [here!]()
 
- You can view the live site [here!]()
+This puzzle is for all ages.
+ 30 Questions About The Coolest And Craziest Facts About The Human Body.
+
+ ![Media]
+
+ ## Navigation
+
+
+      ### There are 3 main navigation buttons:
+    - Start to begin the game 
+    - Restart Game to start over
+    - Next Question to hop over a qustion
+
+        1. As a First Time Visitor, I want to easily understand the main purpose of the site and learn more about the organisation.
+        2. As a First Time Visitor, I want to be able to easily navigate throughout the site to find content.
+        3. As a First Time Visitor, I want to look for testimonials to understand what their users think of them and see if they are trusted. I also want to locate their social media links to see their followings on social media to determine how trusted and known they are.
+
 ## Fonts and icons
 - fonts from google font
 - icons from font awsome
@@ -16,4 +31,4 @@ This puzzle is for all ages.
 - The code for the function to reveal the correct answer was modified from Code Institute's "JavaScript Essentials" course.
 
 ### Media
-- The background image was taken from [Free images](https://freeimages.com/)
+- The background image was taken from [Pixa Bay](https://pixabay.com/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,55 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "answer") {
+                checkAnswer();
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+    }
+
+    document.getElementById("question").addEventListener("keydown", function (event) {
+        if (event.key === "answer-button") {
+            checkAnswer();
+        }
+    });
+
+    runGame("question");
+
+
+});
+
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+
+ function runGame(gameType) {
+
+// Gets all the elements needed and stores them in variables:
+let quizBox = document.getElementById('question-box');
+let questionH2 = document.getElementById('question');
+let answerButton = document.getElementsByClassName('answer-button');
+let nextButton = document.getElementById('next-button');
+let restartButton = document.getElementById('restart-button');
+let scoreTracker = document.getElementById('score');
+let q = 0;
+let a = 0;
+let correct = 0;
+let totalQuestions = 30;
+let currentQuestion = 0;
+
+scoreTracker.innerHTML = 'Score: ' + correct + '/' + totalQuestions;
+
+
+
 let questions = [
     {
         question: '#1 How much percentage of the human body is constituted by water?',
@@ -21,3 +73,4 @@ let questions = [
         correctAnswer: 'Yes'
     }
 ]
+}
