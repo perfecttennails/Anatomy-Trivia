b384c2f867b28572752456d8d5c9593111a647a5
perfecttennails <bgc.info@gmail.com>
2022-04-16 17:02:33 +0000

    add js and media query update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,47 @@
 // Questions will be asked
 
-    const Questions = [{
-        id: 0,
-        q: "How much percentage of the human body is constituted by water?",
-        a: [{ text: "40%", isCorrect: false},
-            { text: "56%", isCorrect: false},
-            { text: "66%", isCorrect: true},
-            { text: "89%", isCorrect: false},
-        ]
-    },
-    {
-        id: 1,
-        q: "Is the nose of an average human being as long as the length of their thumb?",
-        a: [{ text: "Yes", isCorrect: true},
-            { text: "No", isCorrect: false },
-            { text: "", isCorrect: false },
-            { text: "", isCorrect: false }
-        ]
-    },
-    {
-        id: 2,
-        q: "At what rate does an average human being sneeze?",
-        a: [{ text: "40 miles per hour", isCorrect: false },
-            { text: "80 miles per hour", isCorrect: false },
-            { text: "100 miles per hour", isCorrect: true },
-            { text: "120 miles per hour", isCorrect: false }
-        ]
-  
-    },
-    {
-        id: 3,
-        q: "Cells related to which part of the body divide considerably faster when compared to the rest?",
-        a: [{ text: "Blood", isCorrect: false },
-            { text: "Skin", isCorrect: false },
-            { text: "Hair", isCorrect: true },
-            { text: "Nails", isCorrect: false }
-        ]
-  
-    },
-  
+const Questions = [{
+    id: 0,
+    q: "How much percentage of the human body is constituted by water?",
+    a: [{ text: "40%", isCorrect: false },
+    { text: "56%", isCorrect: false },
+    { text: "66%", isCorrect: true },
+    { text: "89%", isCorrect: false },
+    ]
+},
+{
+    id: 1,
+    q: "Is the nose of an average human being as long as the length of their thumb?",
+    a: [{ text: "Yes", isCorrect: true },
+    { text: "No", isCorrect: false },
+    { text: "", isCorrect: false },
+    { text: "", isCorrect: false }
+    ]
+},
+{
+    id: 2,
+    q: "At what rate does an average human being sneeze?",
+    a: [{ text: "40 miles per hour", isCorrect: false },
+    { text: "80 miles per hour", isCorrect: false },
+    { text: "100 miles per hour", isCorrect: true },
+    { text: "120 miles per hour", isCorrect: false }
+    ]
+
+},
+{
+    id: 3,
+    q: "Cells related to which part of the body divide considerably faster when compared to the rest?",
+    a: [{ text: "Blood", isCorrect: false },
+    { text: "Skin", isCorrect: false },
+    { text: "Hair", isCorrect: true },
+    { text: "Nails", isCorrect: false }
+    ]
+
+},
+
 ]
-  
-  
+
+
 // Set start
 var start = true;
 
@@ -58,98 +58,96 @@ function iterate(id) {
     // Setting the question text
     question.innerText = Questions[id].q;
 
-  
+
     // Getting the options
     const opt1 = document.getElementById("opt1");
     const opt2 = document.getElementById("opt2");
     const opt3 = document.getElementById("opt3");
     const opt4 = document.getElementById("opt4");
 
-// Providing option text 
-opt1.innerText = Questions[id].a[0].text;
-opt2.innerText = Questions[id].a[1].text;
-opt3.innerText = Questions[id].a[2].text;
-opt4.innerText = Questions[id].a[3].text;
+    // Providing option text 
+    opt1.innerText = Questions[id].a[0].text;
+    opt2.innerText = Questions[id].a[1].text;
+    opt3.innerText = Questions[id].a[2].text;
+    opt4.innerText = Questions[id].a[3].text;
 
-// Providing the true or false value to the options
-opt1.value = Questions[id].a[0].isCorrect;
-opt2.value = Questions[id].a[1].isCorrect;
-opt3.value = Questions[id].a[2].isCorrect;
-opt4.value = Questions[id].a[3].isCorrect;
+    // Providing the true or false value to the options
+    opt1.value = Questions[id].a[0].isCorrect;
+    opt2.value = Questions[id].a[1].isCorrect;
+    opt3.value = Questions[id].a[2].isCorrect;
+    opt4.value = Questions[id].a[3].isCorrect;
 
-var selected = "";
+    var selected = "";
 
-// Show selection for op1
-opt1.addEventListener("click", () => {
-    opt1.style.backgroundColor = "yellow";
-    opt2.style.backgroundColor = "white";
-    opt3.style.backgroundColor = "white";
-    opt4.style.backgroundColor = "white";
-    selected = opt1.value;
-})
+    // Show selection for op1
+    opt1.addEventListener("click", () => {
+        opt1.style.backgroundColor = "yellow";
+        opt2.style.backgroundColor = "white";
+        opt3.style.backgroundColor = "white";
+        opt4.style.backgroundColor = "white";
+        selected = opt1.value;
+    })
 
-// Show selection for op2
-opt2.addEventListener("click", () => {
-    opt1.style.backgroundColor = "white";
-    opt2.style.backgroundColor = "yellow";
-    opt3.style.backgroundColor = "white";
-    opt4.style.backgroundColor = "white";
-    selected = opt2.value;
-})
+    // Show selection for op2
+    opt2.addEventListener("click", () => {
+        opt1.style.backgroundColor = "white";
+        opt2.style.backgroundColor = "yellow";
+        opt3.style.backgroundColor = "white";
+        opt4.style.backgroundColor = "white";
+        selected = opt2.value;
+    })
 
-// Show selection for op3
-opt3.addEventListener("click", () => {
-    opt1.style.backgroundColor = "white";
-    opt2.style.backgroundColor = "white";
-    opt3.style.backgroundColor = "yellow";
-    opt4.style.backgroundColor = "white";
-    selected = opt3.value;
-})
+    // Show selection for op3
+    opt3.addEventListener("click", () => {
+        opt1.style.backgroundColor = "white";
+        opt2.style.backgroundColor = "white";
+        opt3.style.backgroundColor = "yellow";
+        opt4.style.backgroundColor = "white";
+        selected = opt3.value;
+    })
 
-// Show selection for op4
-opt4.addEventListener("click", () => {
-    opt1.style.backgroundColor = "white";
-    opt2.style.backgroundColor = "white";
-    opt3.style.backgroundColor = "white";
-    opt4.style.backgroundColor = "yellow";
-    selected = opt4.value;
-})
+    // Show selection for op4
+    opt4.addEventListener("click", () => {
+        opt1.style.backgroundColor = "white";
+        opt2.style.backgroundColor = "white";
+        opt3.style.backgroundColor = "white";
+        opt4.style.backgroundColor = "yellow";
+        selected = opt4.value;
+    })
 
- //Submit for the correct answer
-const submitAnswer = document.getElementsByClassName("btn-submit");
+    //Submit for the correct answer
+    const submitAnswer = document.getElementsByClassName("btn-submit");
 
-submitAnswer[0].addEventListener("click", () => {
-   if (selected == "true") {
-       gameArea[0].innerText = "True";
-       gameArea[0].style.backgroundColor = "lightgreen";
-       correct[0].style.innerText = "correct";
-       correct++
-       correct.innerText = correct++
-
-    } else {
-       gameArea[0].innerText = "False";
-       gameArea[0].style.backgroundColor = "red";
-       
-    }
-})
+    submitAnswer[0].addEventListener("click", () => {
+        if (selected == "true") {
+            gameArea[0].innerText = "True";
+            gameArea[0].style.backgroundColor = "lightgreen";
+            scoreArea.innerText = "correct";
+            count ++
+        } else {
+            gameArea[0].innerText = "False";
+            gameArea[0].style.backgroundColor = "red";
+            scoreArea.innerText = "incorrect";
+            count ++
+        }
+    })
 }
 //Getting the score
 const scoreArea = document.querySelector("score-area")
 
-
-//initialize the count variable
-let scores = 0;
-let q = 0;
-let a = 0;
+//initialize the counter variable
+let count = 0;
 let correct = 0;
 let incorrect = 0;
 let score = 0;
 let currentQuestion = 0;
+let totalQuestions = 0;
 
+score.innerHTML = 'Score: ' + correct + '/' + totalQuestions;
 
 
 if (start) {
-iterate("0");
+    iterate("0");
 }
 
 // Next button and method
@@ -157,17 +155,17 @@ const nextQuestion = document.getElementsByClassName('btn-next')[0];
 var id = 0;
 
 nextQuestion.addEventListener("click", () => {
-start = false;
-if (id < 3) {
-    id++;
-    iterate(id);
-}
+    start = false;
+    if (id < 3) {
+        id++;
+        iterate(id);
+    }
 })
 // Restart button and method
-document.querySelector('.btn-restart').addEventListener('click', function(){
+document.querySelector('.btn-restart').addEventListener('click', function () {
     window.location.reload();
     return false;
-  });
+});
 
 
-  
+
